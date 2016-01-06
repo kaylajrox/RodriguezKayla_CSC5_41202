5b03b5a6e845ea8aab199025370164dbb4009d2b
kaylajrox <krodriguez106@att.net>
2016-01-06 11:11:01 -0800

    Signed-off-by: kaylajrox <krodriguez106@att.net>

--- Homework/Savitch_9thEd_Chap1_Prob3/nbproject/Makefile-impl.mk
+++ Homework/Savitch_9thEd_Chap1_Prob3/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=CppApplication_1
+PROJECTNAME=Savitch_9thEd_Chap1_Prob3
 
 # Active Configuration
 DEFAULTCONF=Debug
--- Homework/Savitch_9thEd_Chap1_Prob3/nbproject/Makefile-variables.mk
+++ Homework/Savitch_9thEd_Chap1_Prob3/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Debug=cppapplication_1
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/cppapplication_1
+CND_ARTIFACT_NAME_Debug=savitch_9thed_chap1_prob3
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/savitch_9thed_chap1_prob3
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Debug=cppapplication1.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/cppapplication1.tar
+CND_PACKAGE_NAME_Debug=savitch9thedchap1prob3.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/savitch9thedchap1prob3.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Release=cppapplication_1
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/cppapplication_1
+CND_ARTIFACT_NAME_Release=savitch_9thed_chap1_prob3
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/savitch_9thed_chap1_prob3
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Release=cppapplication1.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/cppapplication1.tar
+CND_PACKAGE_NAME_Release=savitch9thedchap1prob3.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/savitch9thedchap1prob3.tar
 #
 # include compiler specific variables
 #
