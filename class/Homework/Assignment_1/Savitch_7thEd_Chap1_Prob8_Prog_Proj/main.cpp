f19b7d8fc1c7fc55b63de2a707417ea1b5983e82
kaylajrox <krodriguez106@att.net>
2016-01-11 10:30:44 -0800

    Signed-off-by: kaylajrox <krodriguez106@att.net>

--- class/Homework/Assignment_1/Savitch_7thEd_Chap1_Prob8_Prog_Proj/main.cpp
+++ class/Homework/Assignment_1/Savitch_7thEd_Chap1_Prob8_Prog_Proj/main.cpp
@@ -1,8 +1,7 @@
-
 /* 
  * Author: Kayla Rodriguez
- * Created on January 6, 2016, 9:38 AM
- * Purpose: Quarters, Nickels, Dimes
+ * Created on January 10, 2016, 9:38 AM
+ * Purpose: Value of Quarters, Nickels, Dimes in cents and dollars
  */
 
 //System Libraries
@@ -12,33 +11,35 @@ using namespace std;
 //User Libraries
 
 //Global Constant
-const char CNVQRTR=25;//Cents per quarter
-const char CNVDIME=10;//Cents per dime
-const char CNVNIKL=5;//Cents per nickel
+const char CNVQRTR=25;   //Cents per quarter
+const char CNVDIME=10;   //Cents per dime
+const char CNVNIKL=5;    //Cents per nickel
 const char CNVPNDL=100;//Conversion from pennies to dollars
+
 //Function Prototypes
 
 //Execution Begins Here
 int main(int argc, char** argv) {
     //Declare and initialize variables
-    unsigned char nQrtrs,nNikls,nDimes;//Declare inputs, number of Q,D,N
+    unsigned short nQrtrs,nNikls,nDimes;//Declare inputs, number of Q,D,N
     unsigned short total;//total
     
     //Input the number of coins 
-    cout<<"How many quarters do you have 0-9"<<endl;
+    cout<<"How many quarters do you have? "<<endl;
     cin>>nQrtrs;
-    cout<<"How many dimes doy ou have 0-9"<<endl;
+    cout<<"How many dimes do you have? "<<endl;
     cin>>nDimes;
-    cout<<"How many nickels do you have 0-9"<<endl;
+    cout<<"How many nickels do you have? "<<endl;
     cin>>nNikls;
     
     //Calculate the total
-    total=(nQrtrs-48)*CNVQRTR+(nDimes-48)*CNVDIME+(nNikls-48)*CNVNIKL;
+    total=nQrtrs*CNVQRTR+nDimes*CNVDIME+nNikls*CNVNIKL;
     //Output the results
     cout<<"Number of Quarters input = "<<nQrtrs<<endl;
     cout<<"Number of Dimes    input = "<<nDimes<<endl;
     cout<<"Number of Nickels  input = "<<nNikls<<endl;
-    cout<<"The dollar amount = $"<<1.0f*total/CNVPNDL<<endl;
+    cout<<"The amount in cents:   "<<1.0f*total<<endl;
+    cout<<"This amount in dollars is: $"<<1.0f*total/CNVPNDL<<endl;
     
     //Exit stage right
     return 0;
