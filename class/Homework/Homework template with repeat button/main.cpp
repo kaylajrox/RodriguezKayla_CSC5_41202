04604e4699b81b173a6812dbb5c8298ffd3b60bb
kaylajrox <krodriguez106@att.net>
2016-01-13 19:07:23 -0800

    Signed-off-by: kaylajrox <krodriguez106@att.net>

---
+++ class/Homework/Homework template with repeat button/main.cpp
@@ -0,0 +1,43 @@
+/* 
+ * Author: Kayla Rodriguez
+ * Created on January 5, 2016, 9:20 PM
+ * Purpose: 
+ */
+ 
+//System Libraries
+#include <iostream>
+using namespace std;
+ 
+//User Libraries
+ 
+//Global Constant
+
+ 
+//Function Prototypes
+ 
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    
+    char choice;
+    do {
+        //Input data
+       
+
+        //Calculate
+        
+
+        //Output
+
+        c
+        cout<<"Would you like to repeat the program? "<<endl;
+        cout<<"y=yes anything else to exit"<<endl;
+        cin>>choice;
+    }while(choice=='y');
+    
+
+    
+    
+    //Exit stage right
+    return 0;
+}
