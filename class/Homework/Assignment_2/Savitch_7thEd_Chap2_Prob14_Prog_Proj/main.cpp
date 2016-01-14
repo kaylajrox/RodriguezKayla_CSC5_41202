0c238ff286130d659a3749dca92a2a5026fa4554
kaylajrox <krodriguez106@att.net>
2016-01-14 09:54:11 -0800

    Signed-off-by: kaylajrox <krodriguez106@att.net>

--- class/Homework/Assignment_2/Savitch_7thEd_Chap2_Prob14_Prog_Proj/main.cpp
+++ class/Homework/Assignment_2/Savitch_7thEd_Chap2_Prob14_Prog_Proj/main.cpp
@@ -18,53 +18,54 @@ using namespace std;
 //Execution Begins Here
 int main(int argc, char** argv) {
     //Declare and initialize variables
-    char instctr; //name of instructor
-    char nme;     //your name
-    char foodnme; //food name
+    string instctr; //name of instructor
+    string nme;     //your name
+    string foodnme; //food name
     unsigned int n;//number between 100 and 120
-    char adj;     //adjective
-    char color;   //color
-    char animal;  //animal
+    string adj;     //adjective
+    string color;   //color
+    string animal;  //animal
     char choice;
     do {
         //Input data
         cout<<"This is a game of Mad Lib! Enter the following to play:"<<endl;
         cout<<"The first or last name of your instructor "<<endl;
-        string mystr;
-        cin>>(instctr, mystr);
+        getline(cin,instctr);
         cout<<"Your name "<<endl;
-        cin>>(nme, mystr);
+        getline(cin,nme);
         cout<<"The name of a food "<<endl;
-        cin>>(foodnme, mystr);
+        getline(cin,foodnme);
         cout<<"A number between 100 and 120 "<<endl;
         cin>>n;
+        cin.ignore(1000,'\n');
         cout<<"An adjective "<<endl;
-        cin>>(adj, mystr);
+        getline(cin,adj);
         cout<<"A color "<<endl;
-        cin>>(color, mystr);
+        getline(cin,color);
         cout<<"An animal "<<endl;
-        cin>>(animal, mystr);
+        getline(cin,animal);
        
         //Calculate
         
 
         //Output
-        cout<<"Dear Instructor "<<mystr<<","<<endl;
+        cout<<"Dear Instructor "<<instctr<<","<<endl;
         cout<<"               "<<endl;
         cout<<"I am sorry that I am unable to turn in my homework at this time."<<endl;
-        cout<<"First, I ate a rotten "<<mystr<<", which made me turn "<<mystr<<" and"<<endl;
+        cout<<"First, I ate a rotten "<<foodnme<<", which made me turn "<<color<<" and"<<endl;
         cout<<"extremely ill. I came down with a fever of "<<n<<"."<<endl;
-        cout<<"Next, my "<<mystr<<" pet "<<mystr<<" must have smelled the remains of the "<<mystr<<endl;
+        cout<<"Next, my "<<adj<<" pet "<<animal<<" must have smelled the remains of the "<<foodnme<<endl;
         cout<<"on my homework, because he ate it. I am currently rewriting my homework and I hope you"<<endl;
         cout<<"accept it late."<<endl;
         cout<<"                "<<endl;
         cout<<"Sincerely,"<<endl;
-        cout<<mystr<<endl;
+        cout<<nme<<endl;
 
         
         cout<<"Would you like to repeat the program? "<<endl;
         cout<<"y=yes anything else to exit"<<endl;
         cin>>choice;
+        cin.ignore(1000,'\n');
     }while(choice=='y');
     
 
