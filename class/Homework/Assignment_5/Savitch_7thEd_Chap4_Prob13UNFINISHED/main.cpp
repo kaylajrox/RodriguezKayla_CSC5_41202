015a831aa0a8860e09766471806f06248bcc6fb3
kaylajrox <krodriguez106@att.net>
2016-02-03 12:02:08 -0800

    Signed-off-by: kaylajrox <krodriguez106@att.net>

---
+++ class/Homework/Assignment_5/Savitch_7thEd_Chap4_Prob13UNFINISHED/main.cpp
@@ -0,0 +1,91 @@
+/* 
+ * File:   main.cpp
+ * Author: Kayla Rodriguez
+ * Created on January 4, 2016, 10:18 AM
+ * Purpose:  Check out IDE
+ */
+
+//System Libraries
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+void sNum(unsigned int,unsigned int,int);
+//Execution Begins Here
+int main(int argc, char** argv) {
+//The problem to solve
+cout<<endl<<"Solution to Savitch 7thEd Chap4 Prob13"<<endl;
+cout<<endl<<"The Roman Numeral Conversion Program"<<endl<<endl;
+    
+//Declare and initialize variables
+unsigned int  n1000s,n100s,n10s,n1s;//Number of 10^x's
+int number;                 //Arabic number, regular year
+//Input
+cout<<"Input a number between 0-99 to see the lyrics of 'Ninety-Nine Bottles of";
+cout<<"Beer on the Wall.' ";
+cin>>number;
+//Output
+for (int n=0;n<=number;number--){
+    if (number!=0){
+        sNum(n10s,n1s,number);
+        cout<<"bottles of beer on the wall, "<<endl;
+        sNum(n10s,n1s,number);
+        cout<<"bottles of beer, "<<endl;
+        cout<<"Take one down, pass it around,"<<endl;
+    }
+sNum(n10s,n1s,number);
+cout<<"bottles of beer on the wall."<<endl;
+}
+    //Exit stage right
+    return 0;
+}
+//000000001111111112222222222333333333344444444445555555555666666666677777777778
+//345678901234567890123456789012345678901234567890123456789012345678901234567890
+/*                        String Written Number Function                      */
+/******************************************************************************/
+void sNum(unsigned int n10s,unsigned int n1s,int number){ 
+//Calculate the number of 1000's,100's,10's,1's
+    if (number!=0){
+        n10s  =(number-number%10)/10;    //Number of 10's
+        n1s   =(number-n10s*10);         //Subtract off 10's to get 1's
+    }else{
+        n10s=n1s=0;
+    }
+        //Output the 10's
+        switch(n10s){
+            case 9:  cout<<"Ninety ";break;
+            case 8:  cout<<"Eighty ";break;
+            case 7:  cout<<"Seventy ";break;
+            case 6:  cout<<"Sixty ";break;
+            case 5:  cout<<"Fifty ";break;
+            case 4:  cout<<"Forty ";break;
+            case 3:  cout<<"Thirty ";break;
+            case 2:  cout<<"Twenty ";break;
+            case 1:  cout<<"Ten ";break; 
+            default:break;
+        }
+
+        //Output the 1's
+        switch(n1s){
+            case 9:  cout<<"Nine ";break;
+            case 8:  cout<<"Eight ";break;
+            case 7:  cout<<"Seven ";break;
+            case 6:  cout<<"Six ";break;
+            case 5:  cout<<"Five ";break;
+            case 4:  cout<<"Four ";break;
+            case 3:  cout<<"Three ";break;
+            case 2:  cout<<"Two ";break;
+            case 1:  cout<<"One ";break;
+            case 0:{
+                if (n10s=0){
+                cout<<"Zero ";break;
+                }
+            }
+            default:break;
+        }
+}
---
+++ class/Homework/Assignment_5/Savitch_7thEd_Chap4_Prob4/main.cpp
@@ -0,0 +1,51 @@
+/* 
+ * File:   main.cpp
+ * Author: Kayla Rodriguez
+ * Created on January 4, 2016, 10:18 AM
+ * Purpose:  Check out IDE
+ */
+
+//System Libraries
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+const char CNVPERC=100;
+//Function prototypes
+void infrate(float,float);
+//Execution Begins Here
+int main(int argc, char** argv) {
+//Declare and initialize variables
+    float pitemO;
+    float pitemN;
+    char choice;
+    do {
+        //Input data
+        cout<<"What is the price of your item one year ago? ";
+        cin>>pitemO;
+        cout<<"What is the price of your item currently?  ";
+        cin>>pitemN;
+
+        //Output the results
+        cout<<fixed<<setprecision(2);
+        infrate(pitemO,pitemN);
+  
+    //Repeat program?
+    cout<<"Would you like to repeat the program? "<<endl;
+    cout<<"y=yes anything else to exit"<<endl;
+    cin>>choice;
+    }while(choice=='y');
+    //Exit stage right
+    return 0;
+}
+//000000001111111112222222222333333333344444444445555555555666666666677777777778
+//345678901234567890123456789012345678901234567890123456789012345678901234567890
+/*                        Inflation Rate Calculation Function                 */
+/******************************************************************************/
+void infrate(float pitemO, float pitemN){ 
+    float infrate=((pitemN-pitemO)/pitemO)*CNVPERC;
+    cout<<"The estimated inflation rate is "<<infrate<<"%"<<endl;
+}
