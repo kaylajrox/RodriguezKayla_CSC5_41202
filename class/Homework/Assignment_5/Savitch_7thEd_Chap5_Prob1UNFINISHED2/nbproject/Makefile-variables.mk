9fb3459d8b1a88735188de4b212dc093402a9d96
kaylajrox <krodriguez106@att.net>
2016-02-03 20:36:36 -0800

    Signed-off-by: kaylajrox <krodriguez106@att.net>

--- class/Homework/Assignment_5/Savitch_7thEd_Chap5_Prob1UNFINISHED2/nbproject/Makefile-variables.mk
+++ class/Homework/Assignment_5/Savitch_7thEd_Chap5_Prob1UNFINISHED2/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
-CND_ARTIFACT_NAME_Debug=cppapplication_8
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/cppapplication_8
+CND_ARTIFACT_NAME_Debug=cppapplication_21
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/cppapplication_21
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
-CND_PACKAGE_NAME_Debug=cppapplication8.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/cppapplication8.tar
+CND_PACKAGE_NAME_Debug=cppapplication21.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/cppapplication21.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_1-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_1-Windows
-CND_ARTIFACT_NAME_Release=cppapplication_8
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_1-Windows/cppapplication_8
+CND_ARTIFACT_NAME_Release=cppapplication_21
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_1-Windows/cppapplication_21
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin_1-Windows/package
-CND_PACKAGE_NAME_Release=cppapplication8.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_1-Windows/package/cppapplication8.tar
+CND_PACKAGE_NAME_Release=cppapplication21.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_1-Windows/package/cppapplication21.tar
 #
 # include compiler specific variables
 #
